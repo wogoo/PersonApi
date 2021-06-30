83c41a70f6f435eaa17f0f59f9f0d1c8919f9e87
root <root@DESKTOP-JMN4L1O.localdomain>
2021-06-30 12:44:22 -0300

    commit

---
+++ src/test/java/one/digitalinnovation/personAPI/PersonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
